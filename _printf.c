1f3b51eaabbaa95055d7610a809d8100469e8e75
root <plaromina1997@gmail.com>
2023-07-18 10:15:37 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed_chars = 0;
+	char ch, *str, c;
+
+	va_start(args, format);
+	while ((ch = *format++) != '\0')
+	{
+	if (ch == '%')
+	{
+		ch = *format++;
+	switch (ch)
+		{
+		case 'c':
+			c = va_arg(args, int);
+			putchar(c);
+			printed_chars++;
+			break;
+		case 's':
+			str = va_arg(args, char*);
+			while (*str != '\0')
+			{
+			putchar(*str);
+			printed_chars++;
+			str++;
+			}
+			break;
+		case '%':
+			putchar('%');
+			printed_chars++;
+			break;
+		default:
+			putchar('%');
+			putchar(ch);
+			printed_chars += 2;
+			break;
+		}
+	}
+	else
+	{
+		putchar(ch);
+		printed_chars++;
+	}
+	}
+	va_end(args);
+	return (printed_chars);
+}
