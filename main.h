125f4e155eca8403000d6fbea0b3e0e6b6cdf198
root <agustinaaraujo1412@gmail.com>
2023-07-18 10:26:21 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _PRINTF
+#define _PRINTF
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+int _printf(const char *format, ...);
+
+#endif
