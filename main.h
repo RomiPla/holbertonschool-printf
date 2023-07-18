adda52f395d296c4ac11b96b41bb8acbbe307a15
root <plaromina1997@gmail.com>
2023-07-18 10:22:22 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stddef.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _puts(char *str);
+int _putchar(char c);
+#endif
