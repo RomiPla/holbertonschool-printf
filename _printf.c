03ad5f106e06dc1d92c886292eaae94a67b906ed
root <plaromina1997@gmail.com>
2023-07-23 19:22:11 -0700

    se agreagan casos bordes

--- _printf.c
+++ _printf.c
@@ -5,56 +5,65 @@ int _printf(const char *format, ...)
     va_list args;
     int printed_chars = 0, d;
     char ch, *str, c;
-
-    va_start(args, format);
-    while ((ch = *format++) != '\0')
+    
+    if (format)
     {
-        if (ch == '%')
-        {
-            ch = *format++;
-            switch (ch)
-            {
-            case 'c':
-                c = va_arg(args, int);
-                _putchar(c);
-                printed_chars++;
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                while (*str != '\0')
-                {
-                    _putchar(*str);
-                    printed_chars++;
-                    str++;
-                }
-                break;
-            case '%':
-                _putchar('%');
-                printed_chars++;
-                break;
-            case 'd':
-            case 'i':
-                d = va_arg(args, int);
-                if (d < 0)
-                {
-                    _putchar('-');
-                    printed_chars++;
-                    d = -d;
-                }
-                printed_chars += print_int(d);
-                break;
-            default:
-                _putchar('%');
-                _putchar(ch);
-                printed_chars += 2;
-                break;
-            }
-        }
-        else
-        {
-            _putchar(ch);
-            printed_chars++;
-        }
+ 	if(args == NULL)
+		return (-1);
+	if(format[0] == '%' &&  format[1] =='\0')
+		return(-1);
+	if(format == NULL)
+		return(-1);
+	va_start(args, format);
+        while ((ch = *format++) != '\0')
+    	{
+        	if (ch == '%')
+        	{
+            		ch = *format++;
+            		switch (ch)
+            		{
+            			case 'c':
+                			c = va_arg(args, int);
+                			_putchar(c);
+                			printed_chars++;
+                			break;
+            			case 's':
+                			str = va_arg(args, char *);
+                			while (*str != '\0')
+                			{
+                    				_putchar(*str);
+                    				printed_chars++;
+                   				str++;
+                			}
+                			break;
+            			case '%':
+                			_putchar('%');
+                			printed_chars++;
+                			break;
+            			case 'd':
+            			case 'i':
+               				d = va_arg(args, int);
+               				if (d < 0)
+               			 	{
+                    				_putchar('-');
+                    				printed_chars++;
+                    				d = -d;
+                			}
+                			printed_chars += print_int(d);
+                			break;
+            			default:
+                			_putchar('%');
+                			_putchar(ch);
+                			printed_chars += 2;
+                			break;
+            		}
+        	}
+        	else
+        	{
+            		_putchar(ch);
+            		printed_chars++;
+        	}
+	}
     }
     va_end(args);
     return printed_chars;
