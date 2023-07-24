6fb0413779550d2aee9cd6352b0608562aec9e1f
root <plaromina1997@gmail.com>
2023-07-23 18:34:34 -0700

    fix: Minor bugs

--- main.h
+++ main.h
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#ifndef _PRINTF
-#define _PRINTF
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <math.h>
-#include <assert.h>
-#include <limits.h>
-int _printf(const char *format, ...);
-
-=======
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stddef.h>
@@ -21,5 +7,5 @@ int _printf(const char *format, ...);
 int _printf(const char *format, ...);
 int _puts(char *str);
 int _putchar(char c);
->>>>>>> e6fcb55b7fa23f923251890aca1e130c22c884e4
+int print_int(int num);
 #endif
